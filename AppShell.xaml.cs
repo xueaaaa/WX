328318dfb7458bc86dc8eae0a9feff4248d4ad6c
xueaaaa <ss.bazunov@gmail.com>
2025-07-20 23:56:54 +0300

    add: location model

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WX
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Models/Location/Location.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WX.Models.Location
+{
+    [ObservableObject]
+    public partial class Location
+    {
+        [ObservableProperty]
+        private string _latitude;
+        [ObservableProperty]
+        private string _longitude;
+        [ObservableProperty]
+        private string _elevation;
+        [ObservableProperty]
+        private string _country;
+    }
+}
