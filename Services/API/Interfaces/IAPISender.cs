491b74810a0e138a76403a498f49b20f5131f538
xueaaaa <ss.bazunov@gmail.com>
2025-07-21 00:51:01 +0300

    add: send method

---
+++ Services/API/Interfaces/IAPISender.cs
@@ -0,0 +1,12 @@
+﻿namespace WX.Services.API.Interfaces
+{
+    public interface IAPISender
+    {
+        public string BaseURL { get; set; }
+        public string CombinedURL { get; set; }
+
+        public Task<string> Send();
+        public void RegisterParameter(string parameterName, string parameterValue);
+        public void UnregisterParameters();
+    }
+}
---
+++ Services/API/LocationAPISender.cs
@@ -0,0 +1,36 @@
+﻿using WX.Services.API.Interfaces;
+
+namespace WX.Services.API
+{
+    public class LocationAPISender : IAPISender
+    {
+        public string BaseURL { get; set; }
+        public string CombinedURL { get; set; }
+        
+        public LocationAPISender(string baseURL)
+        {
+            BaseURL = baseURL;
+            CombinedURL = baseURL;
+        }
+
+        public async Task<string> Send()
+        {
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(CombinedURL);
+            string content = await response.Content.ReadAsStringAsync();
+
+            return content;
+        }
+
+        public void RegisterParameter(string parameterName, string parameterValue)
+        {
+            if (CombinedURL[-1] == BaseURL[-1])
+                CombinedURL += $"{parameterName}={parameterValue}";
+            else
+                CombinedURL += $"&{parameterName}={parameterValue}";
+        }
+
+        public void UnregisterParameters() =>
+            CombinedURL = BaseURL;
+    }
+}
