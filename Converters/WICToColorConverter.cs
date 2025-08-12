fbe4595dfaa151dc84f8fdcfc02b1cd24291e5da
xueaaaa <ss.bazunov@gmail.com>
2025-08-12 18:48:43 +0300

    add: weather colors

---
+++ Converters/WICToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using WX.Models.Weather;
+
+namespace WX.Converters
+{
+    public class WICToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not WeatherInterpretationCode)
+                return Application.Current!.Resources[$"{nameof(WeatherInterpretationCode.ClearSky)}Color"];
+
+            var val = (WeatherInterpretationCode)value;
+
+            return Application.Current!.Resources[$"{Enum.GetName(typeof(WeatherInterpretationCode), val)}Color"];
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
